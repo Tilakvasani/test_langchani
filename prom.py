14e39e39d3407e8764cdfe974e60b10ec720185f
TilakvasaniTilakvasanigit config --global user.email tilakvasani65@gmail.com <rutvivasani26@gmail.com>
2026-03-02 14:21:03 +0530

    prompt json file

--- prom.py
+++ prom.py
@@ -1,7 +1,7 @@
 from langchain_groq import ChatGroq
 from dotenv import load_dotenv
 import streamlit as st
-from langchain_core.prompts import PromptTemplate
+from langchain_core.prompts import PromptTemplate,load_prompt
 
 load_dotenv()
 model = ChatGroq(model="moonshotai/kimi-k2-instruct-0905")
@@ -14,29 +14,14 @@ style_input = st.selectbox( "Select Explanation Style", ["Beginner-Friendly", "T
 
 length_input = st.selectbox( "Select Explanation Length", ["Short (1-2 paragraphs)", "Medium (3-5paragraphs)", "Long (detailed explanation)"] )
 
-template=PromptTemplate(
-    template="""
-    Please summarize the research paper titled "{paper_input}" with the following
-specifications:
-Explanation Style: {style_input}
-Explanation Length: {length_input}
-1. Mathematical Details:
-- Include relevant mathematical equations if present in the paper.
-- Explain the mathematical concepts using simple, intuitive code snippets
-where applicable.
-2. Analogies:
-- Use relatable analogies to simplify complex ideas.
-If certain information is not available in the paper, respond with: "Insufficient
-information available" instead of guessing.
-Ensure the summary is clear, accurate, and aligned with the provided style and
-length.""",input_variables=['paper_input','style_input','length_input']
-)
+template=load_prompt('template.json')
 
-promt=template.invoke({
+
+if st.button("submit"):
+    chain = template | model
+    result= chain.invoke({
     'paper_input':paper_input,
     'style_input':style_input,
     'length_input':length_input
-})
-if st.button("submit"):
-    result=model.invoke(promt)
-    st.text(result.content)
+}) 
+    st.write(result.content)
